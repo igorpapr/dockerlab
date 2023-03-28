c3e94a92fb2bfb4809bca5a5f7a4d4e952871688
ihorpaprotskyi <Ihor.Paprotskyi-external@sema4genomics.com>
2023-03-28 20:23:12 +0300

    Added lint and fixed lint errors

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-"use strict"
+'use strict';
 
 const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = 8080
+const PORT = 8080;
 
-app.get('/test', function (req, res) {
-    console.log('Incoming GET `test` request')
-    res.send('This is a `test` response');
-})
+app.get('/test', function(req, res) {
+  console.log('Incoming GET `test` request');
+  res.send('This is a `test` response');
+});
 
-app.get('/index', function (req, res) {
-    console.log('Incoming GET `index` request')
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
+app.get('/index', function(req, res) {
+  console.log('Incoming GET `index` request');
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
-var server = app.listen(PORT, () => {
-    var host = server.address().address
-    var port = server.address().port
-    console.log(">>> Listening at %s:%s", host, port)
-})
+const server = app.listen(PORT, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('>>> Listening at %s:%s', host, port);
+});
